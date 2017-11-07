bcb444583908277bc8e0859666b5194e2b3a3a50
LuisCasas <sc.luis@gmail.com>
2017-11-07 20:37:42 +0000

    Add error handler to the functions

--- currency.js
+++ currency.js
@@ -2,27 +2,45 @@ const axios = require('axios');
 
 
 const getExchangeRate = async (from, to) => {
-    const response = await axios.get(`https://api.fixer.io/latest?base=${from}`);
-    return response.data.rates[to];
+
+    try{
+        const response = await axios.get(`https://api.fixer.io/latest?base=${from}`);
+        const rate = response.data.rates[to];
+
+        if(rate){
+            return rate;
+        } else {
+            throw new Error(`Unable to find rates for ${from} and ${to}`);
+        }
+
+    } catch(e){
+        throw new Error(`Unable to find rates for ${from} and ${to}`);
+    }
 };
 
 const getCountries = async (currencyCode) => {
-    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
 
-    return response.data.map((country) => country.name);
+    try{
+        const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+        return response.data.map((country) => country.name);
+    } catch (e) {
+        throw new Error(`Unable to get countries that use ${currencyCode}`); 
+    }
 };
 
 const convertCurrency = async (from, to, amount) => {
     const countries = await getCountries(from);
-    const rate = await getExchangeRate(from, to);
-
+    const rate = await getExchangeRate(from, to);    
+    
     const exchangeAmount = amount * rate;
 
     return `${from} ${amount} is worth ${to} ${exchangeAmount}. ${from} currency is used in the following countries: \n ${countries.join('\n')}`;
 };
 
-convertCurrency('EUR','GBP', 10).then((status) => {
+convertCurrency('GBP','EUR', 10).then((status) => {
     console.log(status);
+}).catch((err) => {
+    console.log(err.message);
 });
 
 /*
